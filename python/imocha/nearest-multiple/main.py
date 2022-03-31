bc2d4b77a66cd8aab7de6aa6a95522295110a5e9
HamingnottT <Jnottingham836@yahoo.com>
2022-03-31 17:48:18 -0500

    nearest-multiple complete

--- python/imocha/nearest-multiple/main.py
+++ python/imocha/nearest-multiple/main.py
@@ -1,18 +1,16 @@
 def nearestMultiple(N):
-
-    
-
     #this is default OUTPUT. You can change it.
-
-    result = N
-
-    
-
     #write your Logic here:
 
-    
+    # divide whole number N by 100 or multiply by 0.01
+    # then round to the nearest tenth
 
-    
+    n_round_tenth = round(N*0.01, 1)
+
+    # take the result of the previous method and multiply 100 to return to whole number form
+    n_result = round(n_round_tenth*100)
+
+    result = n_result
 
     return result
 
@@ -21,8 +19,6 @@ def nearestMultiple(N):
 #INPUT [uncomment & modify if required]
 
 N = int(input())
-
-
 
 #OUTPUT [uncomment & modify if required]
 
