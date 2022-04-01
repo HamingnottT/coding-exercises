635a0a1108c7396feb8456c1936b51dd7c20408b
HamingnottT <Jnottingham836@yahoo.com>
2022-04-01 15:31:31 -0500

    space-age-local solved...kinda

--- scala/exercism/space-age-local/src/main/scala/SpaceAge.scala
+++ scala/exercism/space-age-local/src/main/scala/SpaceAge.scala
@@ -1,5 +1,22 @@
-object SpaceAge {
-  def onEarth(): Unit = {
 
+
+object SpaceAge {
+  def pow(x:Double, y:Double) = math.pow(x,y)
+
+  def onEarth(seconds: Double): Unit = {
+    import scala.math
+
+    // days to seconds - found by squaring 60 min to 60 sec, then multiplying the exponential product by 24 hours
+    val secondsInYear = 365.25 * ((pow(60,2)) * 24)
+
+    // format the result to return rounded double
+    // val solution = "%.2f".format(seconds/secondsInYear).toDouble
+    val solution = seconds/secondsInYear
+
+    print(solution)
   }
+
+//   def main(args:Array[String]) : Unit = {
+//     onEarth(1000000000)
+//   }
 }
