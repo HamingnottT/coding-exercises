9e5ce9a8e147f9f13e7b44d8acff9ba600cd33ab
HamingnottT <Jnottingham836@yahoo.com>
2022-04-01 11:34:17 -0500

    game-winner solved

--- python/imocha/game-winner/main.py
+++ python/imocha/game-winner/main.py
@@ -1,18 +1,13 @@
 def gameWinner(N, A, B):
     
-    print(A)
-    print(B)
+    # print(A)
+    # print(B)
 
     counter = 0
     A_score = 0
     B_score = 0
-
-    
-    # print(A[len(A)-1])
-
-    # for A[i] in A:
-    #     A[i-1]
-    #     A_score += 1
+    winning_list = ""
+    result = ""
 
     while (counter != N):
         counter += 1
@@ -27,36 +22,35 @@ def gameWinner(N, A, B):
         else:
             pass
 
-    print(A_score)
-    print(B_score)
+    if (A_score > B_score):
+        winning_list = "A"
+        result = f"{winning_list} {A_score}"
+    elif (B_score > A_score):
+        winning_list = "B"
+        result = f"{winning_list} {B_score}"
+    else: 
+        winning_list = "tied"
+        result = winning_list
 
-    result = "placeholder"
     return result
 
 # INPUT [uncomment & modify if required]
 
-# N = int(input())
-N = 5
+N = int(input())
+# N = 5
 
-# temp = input().split()
-temp = "4 7 3 2 9".split()
+temp = input().split()
+# temp = "4 7 3 2 9".split()
+
 A = []
 for i in range(N):
     A.append(int(temp[i]))
 
-# temp_1 = input().split()
-temp_1 = "6 9 2 6 3".split()
+temp_1 = input().split()
+# temp_1 = "6 9 2 6 3".split()
 B = []
 for i in range(N):
     B.append(int(temp_1[i]))
 
 # OUTPUT [uncomment & modify if required]
 print(gameWinner(N, A, B))
-
-# def sortArray(N, A):
-#     # this is default OUTPUT. You can change it.
-#     result = -404
-
-#     # write your Logic here:
-
-#     return result
