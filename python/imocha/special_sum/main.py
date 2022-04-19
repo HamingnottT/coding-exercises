8aadebbac003b566101a5443159d3c47c4504812
HamingnottT <Jnottingham836@yahoo.com>
2022-04-19 11:16:07 -0500

    add SpecialSum

---
+++ python/imocha/special_sum/main.py
@@ -0,0 +1,37 @@
+def SpecialSum(N, Arr, K):
+    
+    def split_list_from_string(string):
+        string_list = string.split(" ")
+        return string_list
+    
+    list = split_list_from_string(Arr)
+
+    def list_string_2_int(list):
+        new_list = []
+        for i in range(0, len(list)):
+            new_list.append(int(list[i]))
+        return new_list
+
+    int_list = list_string_2_int(list)
+
+    first_n = int_list[:(K)]
+
+    del int_list[(K):(K+K)]
+
+    sum(int_list)
+
+    
+    # for debugging
+    print(first_n)
+    print(sum(int_list))
+    pass
+
+def main():
+    N = 14
+    Arr = "1 5 9 7 8 6 4 3 2 1 0 5 8 7"
+    K = 5
+
+    SpecialSum(N, Arr, K)
+
+if __name__ == '__main__':
+    main()
