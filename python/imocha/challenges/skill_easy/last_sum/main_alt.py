a12aa642dc507d407402ad8123af35ac2793e50a
HamingnottT <Jnottingham836@yahoo.com>
2022-05-02 11:52:28 -0500

    add main_alt.py

---
+++ python/imocha/challenges/skill_easy/last_sum/main_alt.py
@@ -0,0 +1,37 @@
+# alternative way of expressing lastSum's recursion
+# this is closer to the predefined boilerplate with now a recursive function called to convert A to a string list
+
+def lastSum(N, A):
+    A_to_stringlist = []
+
+    # recursive call to convert A to a string list
+    # integers cannot normally be iterated through
+    def AToStringlist(N, A_input, Accum):
+        if (N == 0): return Accum.split()
+        else: return AToStringlist(N - 1, A_input, str(A_input[N-1]) + " " + Accum)
+
+    A_to_stringlist = AToStringlist(N, A, "")
+
+    def lastSumHelper(N, stringList, Accum):
+        if (N == 0): return Accum
+        
+        # recursively calls through stringlist
+        # accumulator stores the last digit of each list index by converting the digit back to int type
+        else: return lastSumHelper(N - 1, stringList, int((stringList[N - 1])[-1]) + Accum)
+
+    result = lastSumHelper(N, A_to_stringlist, 0)
+    return result
+   
+
+
+# INPUT [uncomment & modify if required]
+
+N = int(input())
+
+temp = input().split()
+A = []
+for i in range(N):
+    A.append(int(temp[i]))
+
+# OUTPUT [uncomment & modify if required]
+print(lastSum(N,A))
