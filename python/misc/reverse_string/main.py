37d9705ab5af6177c09c8219492fa8754e0c8711
HamingnottT <Jnottingham836@yahoo.com>
2022-05-03 10:38:52 -0500

    edit reversed_string

--- python/misc/reverse_string/main.py
+++ python/misc/reverse_string/main.py
@@ -3,13 +3,13 @@
 def reverse_string(n, string, accumulator):
     if n == 0: return accumulator
     else:
-        print(n)
         return reverse_string(n - 1, string, string[-n] + accumulator)
 
 
 def main():
     string = "Hello"
-    print(reverse_string(len(string), string, ""))
+    print(f"Original string: {string}")
+    print(f"Reversed string: {reverse_string(len(string), string, '')}")
 
 
 if __name__ == '__main__':
