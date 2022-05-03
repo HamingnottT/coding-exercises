108f06cafa03d634d014e683061484a5d9e7b876
HamingnottT <Jnottingham836@yahoo.com>
2022-05-03 11:22:38 -0500

    add misc exercise

---
+++ python/misc/perfect_square/main.py
@@ -0,0 +1,31 @@
+# TO-DO
+
+# 1) memorize the first 10 squares
+#   0^2 = 0
+#   1^2 = 1
+#   2^2 = 4
+#   3^2 = 9
+#   4^2 = 16
+#   5^2 = 25
+#   6^2 = 36
+#   7^2 = 49
+#   8^2 = 64
+#   9^2 = 81
+# 2) separate out their last digit into list
+#   [0, 1, 4, 9, 6, 5, 6, 9, 4, 1]
+#   [0, 1, 4, 9, 6, 5] - w/o duplicates
+# 3) Iterate through each digit of the number to square root
+#   conditionals based on squares list
+
+
+
+def perfect_square(n, ):
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
