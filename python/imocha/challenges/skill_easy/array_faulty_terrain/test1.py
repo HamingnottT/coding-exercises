a65e42db75504d58e7f7266e25dee9dd56acae47
HamingnottT <Jnottingham836@yahoo.com>
2022-05-17 10:01:58 -0500

    add test1

---
+++ python/imocha/challenges/skill_easy/array_faulty_terrain/test1.py
@@ -0,0 +1,27 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+def removeCoach(n, k, a):
+        
+
+
+
+    result=n, k, a
+
+    return result
+
+    #INPUT [uncomment & modify if required]
+
+def main():
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    print(removeCoach(n, k, a))
+
+if __name__ == '__main__':
+    main()
